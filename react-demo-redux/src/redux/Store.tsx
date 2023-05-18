7b171f14b4abade776c417c87e41be891fee90ee
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-18 13:14:04 +0530

    implement store file for active redux-devtools in google chrome

--- react-demo-redux/src/redux/Store.tsx
+++ react-demo-redux/src/redux/Store.tsx
@@ -1,8 +1,9 @@
 import {applyMiddleware, createStore} from "redux";
 import rootReducer from "./rootReducer";
 import {logger} from 'redux-logger';
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-const store:any = createStore(rootReducer,applyMiddleware(logger))
+const store:any = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger)))
 
 export default store
 

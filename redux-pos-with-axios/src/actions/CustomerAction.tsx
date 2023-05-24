9672b007db9f90eb923db5acb11f8697446981e2
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-24 21:38:57 +0530

    compeleted redux customer save action

--- redux-pos-with-axios/src/actions/CustomerAction.tsx
+++ redux-pos-with-axios/src/actions/CustomerAction.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {CUSTOMER_SAVE} from "./CustomerActionTypes";
+import {CUSTOMER_GET, CUSTOMER_SAVE} from "./CustomerActionTypes";
 axios.defaults.baseURL ='https://jsonplaceholder.typicode.com'
 export const saveCustomer=(customer:any)=>{
     return(dispatch:any)=>{
@@ -11,15 +11,32 @@ export const saveCustomer=(customer:any)=>{
             data:customer
         })
             .then((response:any)=>{
-                console.log(response)
+                console.log(response.data)
                 dispatch({
                     type:CUSTOMER_SAVE,
-                    payload:[response]
+                    payload:response.data
                 })
             })
             .catch((error:any)=>{
                 console.log(error.message)
             })
     }
+}
+
+export const getCustomer=(dispatch:any)=>{
+    axios({
+        url:'/posts',
+        method:'GET',
+        responseType:'json'
+    })
+        .then((response:any)=>{
+            dispatch({
+                type:CUSTOMER_GET,
+                payload:response.data
+            })
+        })
+        .catch((error:any)=>{
+            console.log(error.message)
+        })
 }
 
--- redux-pos-with-axios/src/component/CustomerTableComp.tsx
+++ redux-pos-with-axios/src/component/CustomerTableComp.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
+interface CustomerTableProps {
 
+}
+interface CustomerTableState {
 
-class CustomerTableComp extends React.Component {
+}
+class CustomerTableComp extends React.Component<CustomerTableProps,CustomerTableState> {
+    
+    constructor(props: Readonly<CustomerTableProps> | CustomerTableProps) {
+        super(props);
+    }
+
     render() {
         return (
             <div></div>

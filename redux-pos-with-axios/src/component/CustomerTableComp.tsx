222fc135c97470de26a63739e177a317a37b3166
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-24 21:44:26 +0530

    implement customer table component

--- redux-pos-with-axios/src/component/CustomerTableComp.tsx
+++ redux-pos-with-axios/src/component/CustomerTableComp.tsx
@@ -1,22 +1,53 @@
 import React from 'react';
+import {Table} from "antd";
+import {connect} from "react-redux";
 
 interface CustomerTableProps {
-
+    customers?:any
 }
 interface CustomerTableState {
 
 }
 class CustomerTableComp extends React.Component<CustomerTableProps,CustomerTableState> {
-    
+
     constructor(props: Readonly<CustomerTableProps> | CustomerTableProps) {
         super(props);
+        this.state={
+
+        }
     }
 
     render() {
+
+        let columns:any=[
+            {
+                title:'id',
+                dataIndex:'dataIndex'
+            },
+            {
+                title: 'title',
+                dataIndex: 'title'
+            },
+            {
+                title: 'message',
+                dataIndex: 'body'
+            }
+        ]
+
         return (
-            <div></div>
+            <div>
+                <Table
+                columns={columns}
+                />
+            </div>
         );
     }
 }
 
-export default CustomerTableComp;
+const mapToProps=(state:any)=>{
+    return{
+        customers:state.customers
+    }
+}
+
+export default connect(mapToProps)(CustomerTableComp);
